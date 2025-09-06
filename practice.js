3ed874de45d4dbd76f8caaab3c2dcc84653e762c
Pavan@42 <pandipavan48@gmail.com>
2025-09-06 12:35:55 +0530

    mouli talks

--- practice.js
+++ practice.js
@@ -1,1 +1,34 @@
 console.log(" little hearts")
+
+let name ="little hearts";
+console.log(name)
+
+let num = 42;
+console.log(num)
+
+let y=null;
+console.log(y)
+
+
+let islogged =false;
+console.log(islogged)
+
+
+console.log( typeof name)
+console.log( typeof num)
+console.log(typeof islogged)
+console.log(typeof X)
+console.log(typeof null)
+
+let arr =[1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person ={
+    name:"pavan",
+    age:22
+}
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
+console.log(typeof age)
